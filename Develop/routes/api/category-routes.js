19d814a6b8263150951546cbc493d6e5e2baa387
Mark T <aydenthos@gmail.com>
2023-09-05 13:34:14 -0700

    error handling with npm install and sequelize

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const sequelize = require('../../config/connection');
 const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
@@ -7,22 +8,39 @@ router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
   try {
-  const categoryData = await Category.findAll({
-    include: [{ model: Product }],
-  });
-  res.status(200).json(categoryData);
-} catch (err) {
-  res.status(500).json(err);
-}
+    const categoryData = await Category.findAll({
+      include: [{ model: Product }],
+    });
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: [{ model: Product }],
+    });
+    if (!categoryData) {
+      res.status(404).json({ message: 'No category found with that id!' });
+      return;
+    }
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create(req.body)
+  .then((category) => {
+    res.status(200).json(category);
+  }
+  )
 });
 
 router.put('/:id', (req, res) => {
